ce939a24446b2fd0d5288e4a4ee8e371d6a7e6c8
izzat <izzatdev1998@mail.ru>
2022-05-29 21:53:18 +0300

    restylie some elements

--- js/app.js
+++ js/app.js
@@ -2,7 +2,12 @@
 window.addEventListener('DOMContentLoaded', () => {
   const tabs = document.querySelectorAll('.tab__nav-link'),
     tabContent = document.querySelectorAll('.tab__content'),
-    headerParents = document.querySelector('.tab__nav');
+    headerParents = document.querySelector('.tab__nav'),
+    burgerMenu = document.querySelector('.burger__menu'),
+    headerTopNav = document.querySelector('.header__top-nav'),
+    closeBtn = document.querySelector('.close__btn'),
+    smBurgerMenu = document.querySelector('.sm__burger-menu'),
+    headerTop = document.querySelector('header__top');
 
   function hideTabContent() {
     tabContent.forEach(item => {
@@ -28,5 +33,17 @@ window.addEventListener('DOMContentLoaded', () => {
         }
       })
     }
+  })
+
+  smBurgerMenu.addEventListener('click', ()=>{
+    headerTopNav.classList.add('active')
+    headerTop.style.display = 'flex'
+  })
+
+  burgerMenu.addEventListener('click', () => {
+    headerTopNav.classList.add('active')
+  })
+  closeBtn.addEventListener('click', () => {
+    headerTopNav.classList.remove('active')
   })
 })
